21fb1f00abc53a110bac29995d63d8e61bc0cf91
jbrownssf <jpbrown@softstackfactory.org>
2017-03-09 22:44:15 +0000

    Updated then reverted ionic-angular module

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -129,3 +129,135 @@ export class ResultsPage {
 
 
 }
+import { Component } from '@angular/core';
+import { NavController, NavParams, App, AlertController, ToastController} from 'ionic-angular';
+//import { Chart } from 'chart.js';
+//Pages
+import { QuestionsReviewPage } from '../questions-review/questions-review';
+//Providers
+import { UserService} from '../../providers/user-service';
+import { TestService} from '../../providers/test-service';
+
+@Component({
+  selector: 'page-results',
+  templateUrl: 'results.html'
+})
+export class ResultsPage {
+
+  // ChartsJs 
+  // @ViewChild('doughnutCanvas') doughnutCanvas;
+
+  doughnutChart: any;
+  showResults: boolean;
+  loadProgress: number;
+  userId: string = "";
+  userToken: string = "";
+  completedTests: any[] = [];
+  questions: any[] = [];
+
+  constructor(public app: App, public navCtrl: NavController, public navParams: NavParams,
+    public userService: UserService, public testService: TestService, public alertCtrl: AlertController,
+    public toastCtrl: ToastController)
+  {
+    this.userId = window.localStorage.getItem('userId');
+    this.userToken = window.localStorage.getItem('userToken');
+
+    this.testService.getCompletedTests( this.userId, this.userToken )
+      .map(res => res.json())
+      .subscribe(res => {
+        console.log("did enter: ", res)
+        for (let i = 0; i < res.length; i++){
+          if(res[i].userAnswerIds.length === res[i].totalCount){
+            this.completedTests.push(res[i]);
+          }
+        }
+        for (let i = 0; i < this.completedTests.length; i++){
+          this.loadProgress = 100*this.completedTests[i].totalCorrect / this.completedTests[i].totalCount;
+          this.completedTests[i].loadProgress = this.loadProgress;
+        }
+        console.log("tests done", this.completedTests)
+      }, err => {
+       this.answersError()
+      })
+      console.log("Results constructor")
+
+  }
+
+  ionViewWillEnter() {
+    if(this.completedTests.length == 0) {
+      this.showResults = false;
+      console.log('ionViewWillEnter');
+    }
+  }
+
+  noResults() {
+    let alert = this.alertCtrl.create({
+      title: "Doesn't look like you've taken any test.",
+      subTitle: "After completing a test you'll be able to review the results here." ,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  //Doughnut chart
+  // drawChart(correct, total) {
+  //   return this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+  //     type: 'doughnut',
+  //     data: {
+  //       labels: ["Right", "Wrong"],
+  //       datasets: [{
+  //         label: '# of Votes',
+  //         data: [correct, total],
+  //         backgroundColor: [
+  //           'rgba(30, 215, 96, 0.2)',
+  //           'rgba(255, 99, 132, 0.2)'
+  //         ],
+  //         hoverBackgroundColor: [
+  //           "#08f95e",
+  //           "#FF6384"
+  //         ]
+  //       }]
+  //     }
+  //   });
+  // }
+
+  reviewTest(test){
+    this.testService.getQuestions(test.testId, this.userToken)
+    .map(res => res.json())
+      .subscribe(res => {
+        this.questions = res[0].questionIds;
+        this.testService.getUserAnswers(test.id, this.userToken)
+          .map(res => res.json())
+          .subscribe(res => {
+            
+            //this.navCtrl.push(QuestionsReviewPage, {
+             this.app.getRootNav().push(QuestionsReviewPage, {
+
+              "questions": this.questions,
+              "answers": res,
+              "testTakenId": test.testId,
+              "testTitle": test.title,
+              "totalCorrect": test.totalCorrect,
+              "purpose": test.purpose
+            })
+          }, err => {
+            this.answersError();
+          })
+      }, err => {
+        this.answersError();
+      })
+
+  }
+
+  //Alert for if error getting answers
+  answersError() {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Something Went Wrong Please Try Again.',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+
+}
